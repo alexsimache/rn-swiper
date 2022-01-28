950986984159d4cadedecee3c893b617a043bf3b
Alexandru Simache <alex@Alexandrus-MacBook-Pro.local>
2022-01-28 10:21:23 +0200

    first commit

---
+++ src/CustomSwiper.tsx
@@ -0,0 +1,105 @@
+import React, { useCallback, useRef, useState } from 'react';
+import { FlatList, Dimensions, StyleSheet, View } from 'react-native';
+import * as PropTypes from 'prop-types';
+import Pagination from './Pagination';
+
+const CustomSwiper = ({ height, dotColor, activeDotColor, showsPagination, data, renderSlide }) => {
+  const { width: windowWidth } = Dimensions.get('window');
+
+  const styles = StyleSheet.create({
+    carousel: { flex: 1 },
+    slide: {
+      height,
+      width: windowWidth,
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+    },
+  });
+  const [index, setIndex] = useState(0);
+  const indexRef = useRef(index);
+  indexRef.current = index;
+  const onScroll = useCallback((event) => {
+    const slideSize = event.nativeEvent.layoutMeasurement.width;
+    // eslint-disable-next-line no-shadow
+    const index = event.nativeEvent.contentOffset.x / slideSize;
+    const roundIndex = Math.round(index);
+
+    const distance = Math.abs(roundIndex - index);
+
+    // Prevent one pixel triggering setIndex in the middle
+    // of the transition. With this we have to scroll a bit
+    // more to trigger the index change.
+    const isNoMansLand = distance > 0.4;
+
+    if (roundIndex !== indexRef.current && !isNoMansLand) {
+      setIndex(roundIndex);
+    }
+  }, []);
+
+  const flatListOptimizationProps = {
+    initialNumToRender: 0,
+    maxToRenderPerBatch: 1,
+    removeClippedSubviews: true,
+    scrollEventThrottle: 16,
+    windowSize: 2,
+    keyExtractor: useCallback((s) => String(s.id), []),
+    getItemLayout: useCallback(
+      (_, eq) => ({
+        index: eq,
+        length: windowWidth,
+        offset: eq * windowWidth,
+      }),
+      [],
+    ),
+  };
+
+  const renderItem = ({ item }) => {
+    return <View style={[styles.slide]}>{renderSlide({ item })}</View>;
+  };
+
+  const renderFooter = () => {
+    if (!showsPagination || data.length === 1) return null;
+    return (
+      <View style={{ marginTop: 0 }}>
+        <Pagination index={index} data={data} dotColor={dotColor} activeDotColor={activeDotColor} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={{ height: height + 10 }}>
+      <FlatList
+        data={data}
+        style={styles.carousel}
+        renderItem={renderItem}
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        onScroll={onScroll}
+        {...flatListOptimizationProps}
+      />
+      {renderFooter()}
+    </View>
+  );
+};
+CustomSwiper.defaultProps = {
+  height: 100,
+  dotColor: 'gray',
+  activeDotColor: 'lightblue',
+  showsPagination: true,
+  data: [],
+  renderSlide: () => {},
+};
+
+CustomSwiper.propTypes = {
+  height: PropTypes.number,
+  dotColor: PropTypes.string,
+  activeDotColor: PropTypes.string,
+  showsPagination: PropTypes.bool,
+  data: PropTypes.arrayOf(PropTypes.shape({})),
+  renderSlide: PropTypes.func,
+};
+
+export default CustomSwiper;
---
+++ src/Pagination.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, View } from 'react-native';
+import React, { memo } from 'react';
+import * as PropTypes from 'prop-types';
+
+const Pagination = ({ data, index, dotColor, activeDotColor }) => {
+  const styles = StyleSheet.create({
+    pagination: {
+      width: '100%',
+      justifyContent: 'center',
+      flexDirection: 'row',
+      zIndex: 9999,
+    },
+    paginationDot: {
+      width: 8,
+      height: 8,
+      borderRadius: 4,
+      marginHorizontal: 2,
+    },
+    paginationDotActive: { backgroundColor: activeDotColor },
+    paginationDotInactive: { backgroundColor: dotColor },
+  });
+  return (
+    <View style={styles.pagination} pointerEvents="none">
+      {data.map((item, i) => {
+        return (
+          <View
+            key={item.id.toString()}
+            style={[
+              styles.paginationDot,
+              index === i ? styles.paginationDotActive : styles.paginationDotInactive,
+            ]}
+          />
+        );
+      })}
+    </View>
+  );
+};
+
+Pagination.defaultProps = {
+  dotColor: 'gray',
+  activeDotColor: 'lightblue',
+};
+
+Pagination.propTypes = {
+  index: PropTypes.number.isRequired,
+  data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  dotColor: PropTypes.string,
+  activeDotColor: PropTypes.string,
+};
+
+export default memo(Pagination);
