beb4370e0a4555fab5f5b74292bfaf7eea857901
Alexandru Simache <alex@Alexandrus-MacBook-Pro.local>
2022-02-20 18:34:50 +0200

    fast update

--- src/CustomSwiper.tsx
+++ src/CustomSwiper.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef, useState } from 'react';
-import { Dimensions, FlatList, StyleSheet, View } from 'react-native';
+import { Animated, Dimensions, FlatList, Platform, StyleSheet, View } from 'react-native';
 import Pagination from './Pagination';
 
 type defaultPropTypes = {
@@ -20,6 +20,11 @@ const CustomSwiper = ({
                         renderSlide,
                       }: defaultPropTypes) => {
   const { width: windowWidth } = Dimensions.get('window');
+  const [scrollViewWidth, setScrollViewWidth] = React.useState(0);
+  const boxWidth = scrollViewWidth * 0.9;
+  const boxDistance = scrollViewWidth - boxWidth;
+  const halfBoxDistance = boxDistance / 2;
+  const pan = React.useRef(new Animated.ValueXY()).current;
 
   const styles = StyleSheet.create({
     carousel: { flex: 1 },
@@ -34,24 +39,55 @@ const CustomSwiper = ({
   const [index, setIndex] = useState(0);
   const indexRef = useRef(index);
   indexRef.current = index;
-  const onScroll = useCallback((event: { nativeEvent: { layoutMeasurement: { width: any; }; contentOffset: { x: number; }; }; }) => {
-    const slideSize = event.nativeEvent.layoutMeasurement.width;
-    // eslint-disable-next-line no-shadow
-    const index = event.nativeEvent.contentOffset.x / slideSize;
-    const roundIndex = Math.round(index);
+  // onScroll={Animated.event(
+  //   [{ nativeEvent: { contentOffset: { x: pan.x } } }],
+  //   {
+  //     useNativeDriver: false,
+  //   },
+  // )}
+  // const onScroll = useCallback((event: { nativeEvent: { layoutMeasurement: { width: any; }; contentOffset: { x: number; }; }; }) => {
+  //   const slideSize = event.nativeEvent.layoutMeasurement.width;
+  //   // eslint-disable-next-line no-shadow
+  //   const index = event.nativeEvent.contentOffset.x / slideSize;
+  //   const roundIndex = Math.round(index);
+  //
+  //   const distance = Math.abs(roundIndex - index);
+  //
+  //   const preventPixelTriggering = distance > 0.4;
+  //
+  //   if (roundIndex !== indexRef.current && !preventPixelTriggering) {
+  //     setIndex(roundIndex);
+  //   }
+  //   Animated.event(
+  //     [{ nativeEvent: { contentOffset: { x: pan.x } } }],
+  //     {
+  //       useNativeDriver: false,
+  //     },
+  //   )
+  // }, []);
 
-    const distance = Math.abs(roundIndex - index);
-
-    const preventPixelTriggering = distance > 0.4;
-
-    if (roundIndex !== indexRef.current && !preventPixelTriggering) {
-      setIndex(roundIndex);
-    }
-  }, []);
-
-  const renderItem = ({ item }: any) => {
-    return <View style={ [styles.slide] }>{ renderSlide({ item }) }</View>;
-  };
+  const renderItem = ({ item, index }: any) => (
+    <Animated.View
+      style={{
+        transform: [
+          {
+            scale: pan.x.interpolate({
+              inputRange: [
+                (index - 1) * boxWidth - halfBoxDistance - 150,
+                index * boxWidth - halfBoxDistance,
+                (index + 1) * boxWidth - halfBoxDistance + 150, // adjust positioning
+              ],
+              outputRange: [0.90, 1, 0.90], // scale down when out of scope
+              extrapolate: 'clamp',
+            }),
+          },
+        ],
+      }}>
+      <View style={{ height: '100%', width: boxWidth }}>
+        { renderSlide({ item }) }
+      </View>
+    </Animated.View>
+  );
 
   const renderFooter = () => {
     if (!showsPagination || data.length <= 1) return null;
@@ -80,14 +116,49 @@ const CustomSwiper = ({
         horizontal
         showsHorizontalScrollIndicator={ false }
         bounces={ false }
-        onScroll={ onScroll }
+        // onScroll={ onScroll }
         initialNumToRender={ 0 }
-        maxToRenderPerBatch={ 1 }
+        maxToRenderPerBatch={ 2 }
         removeClippedSubviews
-        scrollEventThrottle={ 16 }
         windowSize={ 2 }
         keyExtractor={ keyExtractor }
         getItemLayout={ getItemLayout }
+        contentContainerStyle={{ paddingVertical: 20, paddingHorizontal: Platform.OS === 'ios' ? 0 : 20 }}
+        contentInsetAdjustmentBehavior="never"
+        snapToAlignment="center"
+        decelerationRate="fast"
+        automaticallyAdjustContentInsets={false}
+        showsVerticalScrollIndicator={false}
+        scrollEventThrottle={1}
+        snapToInterval={boxWidth}
+        contentInset={{
+          left: halfBoxDistance,
+          right: halfBoxDistance,
+        }}
+        contentOffset={{ x: halfBoxDistance * -1, y: 0 }}
+        onLayout={(e) => {
+          setScrollViewWidth(e.nativeEvent.layout.width);
+        }}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: pan.x } } }],
+          {
+            useNativeDriver: false,
+            listener: (event: any) => {
+              const slideSize = event.nativeEvent.layoutMeasurement.width;
+              // eslint-disable-next-line no-shadow
+              const index = event.nativeEvent.contentOffset.x / slideSize;
+              const roundIndex = Math.round(index);
+
+              const distance = Math.abs(roundIndex - index);
+
+              const preventPixelTriggering = distance > 0.4;
+
+              if (roundIndex !== indexRef.current && !preventPixelTriggering) {
+                setIndex(roundIndex);
+              }
+            },
+          },
+        )}
       />
       { renderFooter() }
     </View>
