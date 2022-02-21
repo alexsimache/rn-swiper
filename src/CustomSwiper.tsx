91c7f1d65ac03b2b59b241d23ef797969db06b44
Alexandru Simache <alex@Alexandrus-MacBook-Pro.local>
2022-02-21 10:25:31 +0200

    fast update

--- src/CustomSwiper.tsx
+++ src/CustomSwiper.tsx
@@ -9,8 +9,8 @@ type defaultPropTypes = {
   showsPagination: boolean,
   data: [],
   renderSlide: ({ item }: { item: any; }) => JSX.Element,
-  ListHeaderComponent: () => JSX.Element,
-  ListFooterComponent: () => JSX.Element,
+  ListHeaderComponent: React.ComponentType<any> | React.CElement<any>,
+  ListFooterComponent: React.ComponentType<any> | React.CElement<any>,
 }
 
 const CustomSwiper = ({
